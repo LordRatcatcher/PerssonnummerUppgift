697501a2489f885d546224bd8b8ba5f6f9a1d8da
ÄGARE <GARE@DESKTOP-1SVJT58>
2020-10-28 12:29:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PerssonnummerCheckare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("*****************************");                         
+            Console.WriteLine("*Skriv in ditt perssonnummer*");
+            Console.WriteLine("*****************************");
+            string userinput = "";
+            double Perssonnummer = 0;
+            userinput = Console.ReadLine();
+            Perssonnummer = double.Parse(userinput);
+            
+            if (Perssonnummer < 175301010101 || Perssonnummer > 202001010101)
+            {
+                Console.WriteLine("Men Vafan då detta är ju inte möjligt");
+                
+            }
+
+            Console.ReadKey();
+        
+        }
+        
+        
+    }
+}
