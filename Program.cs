0c05d7ca8594538c32085b6acb98538c762e9087
ÄGARE <GARE@DESKTOP-1SVJT58>
2020-10-31 12:59:09 +0100

    Allt är fixat förutom att fixa så att jag kan kalla på metoden

--- Program.cs
+++ Program.cs
@@ -21,10 +21,13 @@ namespace PerssonnummerCheckare
             string Day = Perssonnummer.Substring(6, 2);
             double DayNumber = Convert.ToInt32(Day);
             string LastPart = Perssonnummer.Substring(8, 4);
-            double LastPartNumbers = Convert.ToInt32(LastPart);            
-            int LeapYearCheck = 0;
-            CalculateLeapYear(LeapYearCheck);
-
+            double LastPartNumbers = Convert.ToInt32(LastPart);
+            bool LeapYearCheck = false;
+            string GenderCheck = Perssonnummer.Substring(10, 1);
+            double GenderCheckNumber = Convert.ToInt32(GenderCheck);
+            
+          
+            
 
             //If statements to check for incorrect inputs//
             if (YearNumber < 1753 || YearNumber > 2020)
@@ -39,27 +42,91 @@ namespace PerssonnummerCheckare
             {
                 Console.WriteLine("För sent datum");
             }
-            if (LeapYearCheck == 1)
+            if (LeapYearCheck == true)
             {
                 if (DayNumber > 29 && MonthNumber == 02)
                 {
                     Console.WriteLine("Något om skottår");
                 }
             }
-            
-            Console.ReadKey();
+            if (LeapYearCheck == false)
+            {
+                if (DayNumber > 28 || DayNumber < 1 && MonthNumber == 02)
+                {
+                    Console.Write("Dåligt Datum");
+                }
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 03)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 30 || DayNumber < 1 && MonthNumber == 04)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 05)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 30 || DayNumber < 1 && MonthNumber == 06)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 07)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 08) 
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 30 || DayNumber < 1 && MonthNumber == 09)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 10)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 30 || DayNumber < 1 && MonthNumber == 11)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }
+            if (DayNumber > 31 || DayNumber < 1 && MonthNumber == 12)
+            {
+                Console.WriteLine("Dåligt Datum");
+            }                        
+            Console.WriteLine("Ditt Perssonnummer är acceptabelt!");
+            if (GenderCheckNumber == 0 || GenderCheckNumber == 2 || GenderCheckNumber == 4 || GenderCheckNumber == 6 || GenderCheckNumber == 8)
+            {
+                Console.WriteLine("Ditt Juridiska kön är kvinna");
+            }
+            if (GenderCheckNumber == 1 || GenderCheckNumber == 3 || GenderCheckNumber == 5 || GenderCheckNumber == 7 || GenderCheckNumber == 9)
+            {
+                Console.WriteLine("Ditt Juridiska kön är man");
+            }   
+                Console.ReadKey();
         }
-        //Calculating if the year is a leap year and returning a bool//
-        static public void CalculateLeapYear(double YearNumber)
+        //Calculating if the year is a leap year and returning a bool true if it is//
+        public static bool CalculateLeapYear(double YearNumber, bool LeapYearCheck)
         {
-            
-            double YearCalculation = YearNumber;
+
+
             if (((YearNumber % 4 == 0) && (YearNumber % 100 != 0)) || (YearNumber % 400 == 0))
             {
-               int LeapYearCheck = 1;
-                
+                Console.WriteLine("Det är ett skottår!");
+                LeapYearCheck = true;
+                return LeapYearCheck;
                 
             }
+            else 
+            {
+                LeapYearCheck = false;
+                return LeapYearCheck;
+
+            }
+
+
         }
         
     }
